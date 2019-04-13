6e630124ab4fc5c2876cf016fb8e1dec662af89a
msomi22 <t_pnjeru@kcbgroup.com>
2019-04-13 23:15:47 +0300

    add interceptor

---
+++ src/main/java/com/github/msomi22/interceptor/ItemInterceptor.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.github.msomi22.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author t_pnjeru
+ *
+ */
+@Component
+public class ItemInterceptor implements HandlerInterceptor{
+
+	 @Override
+	   public boolean preHandle(
+	      HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		 
+		 System.out.println("Pre Handle method is Calling");
+		 
+		  if(request.getAttribute("username").toString().isEmpty()) {
+			  return false;
+		  }
+	      return true;
+	   }
+	   @Override
+	   public void postHandle(
+	      HttpServletRequest request, HttpServletResponse response, Object handler, 
+	      ModelAndView modelAndView) throws Exception {
+		   System.out.println("Post Handle method is Calling");
+	   }
+	   
+	   @Override
+	   public void afterCompletion(HttpServletRequest request, HttpServletResponse response, 
+	      Object handler, Exception exception) throws Exception {
+		   System.out.println("Request and Response is completed");
+	   }
+	   
+}
---
+++ src/main/java/com/github/msomi22/interceptor/ItemInterceptorAppConfig.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.github.msomi22.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author t_pnjeru
+ *
+ */
+public class ItemInterceptorAppConfig extends WebMvcConfigurerAdapter {
+	
+	@Autowired
+	ItemInterceptor itemInterceptor;
+	
+	 @Override
+	   public void addInterceptors(InterceptorRegistry registry) {
+	      registry.addInterceptor(itemInterceptor);
+	   }
+
+}
