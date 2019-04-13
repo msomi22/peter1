7f80e8d3cd08936a9cc5832c3b8f3260b23711e9
msomi22 <t_pnjeru@kcbgroup.com>
2019-04-13 16:28:25 +0300

    initial commit

---
+++ src/main/java/com/github/msomi22/Peter1Application.java
@@ -0,0 +1,15 @@
+package com.github.msomi22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Peter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Peter1Application.class, args);
+		
+		System.out.println("hello peter");
+	}
+
+}
